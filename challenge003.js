977662240d247a23a22e185107a0fdefa3b2239c
Pedro Lopes <pedroflopes1710@gmail.com>
2022-11-17 21:49:48 -0300

    challenge 4

--- challenge003.js
+++ challenge003.js
@@ -3,5 +3,5 @@
 for(var i = 1; i <= 10; i++)
 {
     var row = "7 * " + i + " = " + 7 * i;
-    console.log(row);
+    console.log(row); 
 }
---
+++ challenge004.js
@@ -0,0 +1,16 @@
+//Print all the multipliciation tables with numbers from 1 to 10
+
+for(var i = 1; i <= 10; i++)
+{
+    printTable(i);
+    console.log("");
+}
+
+function printTable(n)
+{
+    for(var i = 1; i <= 10; i++)
+    {
+        var row = n + "*" + i + "=" + n * i;
+        console.log(row);
+    }
+}
